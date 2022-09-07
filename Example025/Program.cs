a6b96286e15cabb46208a74f82a67c4186b4c8e8
Евгений <ivanychcos@gmail.com>
2022-09-07 09:17:59 +0300

    Add example 025

---
+++ Example025/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = int.Parse(Console.ReadLine());
+int product = 1;
+for(int i = 1; i <= b; i++)
+{
+    product = product * a;
+}
+Console.Write(product);
