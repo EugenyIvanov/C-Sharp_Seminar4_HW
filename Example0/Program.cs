8f92b4885d8b082d9abfcc673e2a6b96fd84c2f5
Евгений <ivanychcos@gmail.com>
2022-09-10 13:08:29 +0300

    add example 0

---
+++ Example0/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int [] array = new int[10];
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write(array[i] + " ");
+}
+int indexMax = 0;
+for(int j = 0; j < array.Length; j++)
+{
+    if(array[j] > array[indexMax])
+    {
+        indexMax = j;
+    }
+}
+Console.WriteLine();
+Console.WriteLine(array[indexMax]);
